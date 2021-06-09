40dfa02c214120a574c841b01a1a4074ed6895f0
satoshi <takemaru33ohayo@yahoo.co.jp>
2021-06-10 02:52:25 +0900

    queryの実行

--- index.php
+++ index.php
@@ -26,8 +26,12 @@
     echo 'DB接続エラー' . $e->getMessage();
   }
 
-  $count = $db->exec('INSERT INTO my_items SET maker_id=1, item_name="もも", item_name_kana="モモ" ,price=210, keyword="缶詰,ピンク,甘い"');
-  echo $count . '件のデータを挿入しました';
+  $recodes = $db->query('SELECT * FROM my_items');
+  while($recode = $recodes->fetch()) {
+    print($recode['item_name'] . "\n");
+  }
+  // $count = $db->exec('INSERT INTO my_items SET maker_id=1, item_name="もも", item_name_kana="モモ" ,price=210, keyword="缶詰,ピンク,甘い"');
+  // echo $count . '件のデータを挿入しました';
   ?>
 </pre>
 </main>

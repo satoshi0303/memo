30f511bf82b55f4dc7e1d46c4731bc0c09cdc87f
satoshi <takemaru33ohayo@yahoo.co.jp>
2021-06-18 10:22:53 +0900

    投稿記事に編集機能を追加

--- dbconnect.php
+++ dbconnect.php
@@ -1,7 +1,7 @@
 <?php
-  try {
-    $db = new PDO('mysql:dbname=mydb;host=localhost;port=8889;charset=utf8',
-    'root','root');
-  } catch(PDOException $e) {
-    echo 'DB接続エラー' . $e->getMessage();
-  }
+try {
+    $db = new PDO('mysql:dbname=mydb;host=localhost;port=8889;charset=utf8' , 'root', 'root');
+} catch(PDOException $e) {
+    echo 'DB接続エラー: ' . $e->getMessage();
+}
+?>
--- update_do.php
+++ update_do.php
@@ -17,32 +17,21 @@
 
 <main>
 <h2>Practice</h2>
-
+<pre>
 <?php
 require('dbconnect.php');
 
-$id = $_REQUEST['id'];
-if(!is_numeric($id) || $id<= 0) {
-  print('1以上の数字で指定してください');
-  exit();
-}
 
-
-$memos = $db->prepare('SELECT * FROM memos WHERE id=?');
-$memos->execute(array($id));
-$memo = $memos->fetch();
-?>
-
-<article>
-  <pre><?php print($memo['memo']); ?></pre>
-
-  <a href="update.php?id=<?php print($memo['id']); ?>">編集する</a>
-  |
-  <a href="index.php">戻る</a>
-</article>
+      $statement = $db->prepare('UPDATE memos SET memo=? WHERE id=?');
+      $statement->execute(array($_POST['memo'], $_POST['id']));
 
 
 
+  ?>
+<p>メモの内容を変更しました</p>
+</pre>
+
+<p><a href="index.php">戻る</a></p>
 </main>
 </body>    
 </html>
